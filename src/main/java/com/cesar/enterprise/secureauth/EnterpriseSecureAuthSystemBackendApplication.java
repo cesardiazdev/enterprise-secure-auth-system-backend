2650e002e0e0746a1da07b01e8fb213c45fef40f
cesar.diaz <cesar.diaz@acffaa.gob.pe>
2026-05-10 22:05:57 -0500

    Initial enterprise backend structure

---
+++ src/main/java/com/cesar/enterprise/secureauth/EnterpriseSecureAuthSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cesar.enterprise.secureauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseSecureAuthSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseSecureAuthSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesar/enterprise/secureauth/EnterpriseSecureAuthSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar.enterprise.secureauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseSecureAuthSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
